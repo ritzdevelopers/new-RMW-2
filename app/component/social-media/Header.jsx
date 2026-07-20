d0e8f8ddee1622b1e7b3d70a51c00aa24202ee8b
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-07-20 17:59:36 +0530

    enhance font sizes

--- app/component/social-media/Header.jsx
+++ app/component/social-media/Header.jsx
@@ -8,7 +8,7 @@ function Header({ title }) {
           alt="header"
           className="w-full h-[100px] sm:h-[120px] md:h-[140px] lg:h-[161px] object-cover"
         />
-        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[90%] px-3 text-center text-lg sm:text-xl md:text-2xl lg:text-3xl font-[700] text-black leading-tight">
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[90%] px-3 text-center text-lg sm:text-xl md:text-2xl lg:text-[40px] font-[700] text-black leading-tight">
          {title ? title : "Website Design Portfolio"}
         </h1>
     </div>
--- app/component/social-media/Section10.jsx
+++ app/component/social-media/Section10.jsx
@@ -6,10 +6,10 @@ function Section10() {
   return (
     <section className="flex w-full items-center justify-between bg-[#F4F2F0] px-5 py-[30px] sm:px-8  md:px-10  lg:px-4 md:py-[70px]">
       <div className="flex w-full flex-col items-center justify-center gap-4 text-center">
-        <h2 className="font-[400] text-[22px]  md:text-[24px] lg:text-[28px]">
+        <h2 className="font-[400] text-[22px]  md:text-[24px] lg:text-[35px]">
           Ready to take the next step?
         </h2>
-        <p className="max-w-[600px] font-[300] lg:text-[18px] md:text-[16px] leading-[1.6] lg:leading-normal">
+        <p className="max-w-[600px] font-[300] lg:text-[20px] md:text-[16px] leading-[1.6] lg:leading-normal">
         Let's Create Something Your Brand Will Be Known For
         Have a project in mind? Let's bring your brand's creative vision to life.
         </p>
