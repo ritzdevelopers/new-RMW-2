3d1f3a8d68437473f28d6f6915e0a8bd75495bf4
itzz-aryan-121 <arhanrizvi9@gmail.com>
2026-07-22 17:30:19 +0530

    modify the case study stack hover card width

---
+++ app/component/influencer-marketing/page.jsx
@@ -0,0 +1,9 @@
+function INFSection1() {
+    return (
+        <div>
+            <h1>INFSection1</h1>
+        </div>
+    )
+}
+
+export default INFSection1
---
+++ app/portfolio/infleuncer-marketing-videos/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import INFSection1 from '@/app/component/influencer-marketing/INFSection1'
+
+function page() {
+  return (
+    <>
+    <INFSection1 />
+    </>
+  )
+}
+
+export default page;
