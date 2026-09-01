2558c32fce6cda4cacae74f41beba146c9a652ef
Shivam Singh <sin2000shiv@gmail.com>
2026-09-01 09:44:23 +0530

    fix navigation of contact page from call icon

--- app/component/latest/Footer.jsx
+++ app/component/latest/Footer.jsx
@@ -150,8 +150,8 @@ function ContactFloat() {
   return (
     <div className="fixed bottom-6 right-6 z-[9999] flex flex-col items-center gap-3">
       <div className="relative h-14 w-14">
-        <a
-          href="tel:+919220516777"
+        <Link
+          href="/contact"
           aria-label="Call us"
           onMouseEnter={triggerBurst}
           onPointerDown={triggerBurst}
@@ -166,7 +166,7 @@ function ContactFloat() {
           >
             <path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 00-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" />
           </svg>
-        </a>
+        </Link>
         {bursts.map((id) => (
           <TriangleParticleBurst key={id} burstId={id} onComplete={() => removeBurst(id)} />
         ))}
